dc817d5d718c359f4932185e0bb69dbdd5e73892
Tyler Lartonoix <lord.codemonkey@gmail.com>
2025-03-11 23:10:04 -0400

    Parallelize extractor tool

--- tools/extractor/src/command.rs
+++
@@ -1,9 +0,0 @@
-use std::ffi::OsStr;
-use std::fmt::Debug;
-use std::path::Path;
-use std::process::{Command, Stdio};
-
-pub fn exec<I, S>(path: &Path, args: I) -> bool where I: IntoIterator<Item = S> + Debug, S: AsRef<OsStr> {
-    let status = Command::new(path).args(args).stdout(Stdio::null()).status().unwrap();
-    status.success()
-}
--- tools/extractor/src/main.rs
+++ tools/extractor/src/main.rs
@@ -1,17 +1,18 @@
 use crate::cli::Cli;
-use crate::config::Config;
 use clap::Parser;
 use console::Style;
 use indicatif::ProgressBar;
+use rayon::iter::ParallelIterator;
+use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator};
+use rslib::config::Config;
+use rslib::tools::{MsgExtractor, UserExtractor};
 use std::path::{Path, PathBuf};
 use std::{fs, io};
 use wax::Glob;
 
 mod cli;
-mod command;
-mod config;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     if let Some(cwd) = &cli.cwd {
@@ -19,7 +20,7 @@ fn main() {
     }
 
     let config = Config::load(cli.config.as_deref());
-    run(&cli, &config);
+    run(&cli, &config)
 }
 
 fn setup<P: AsRef<Path>>(
@@ -42,107 +43,84 @@ fn setup<P: AsRef<Path>>(
     Ok((in_dir, out_dir))
 }
 
-fn run(cli: &Cli, config: &Config) {
+fn run(cli: &Cli, config: &Config) -> anyhow::Result<()> {
     let style = Style::new().bold().dim();
 
     if !cli.skip_data {
-        let progress = ProgressBar::new(config.extract.files.len() as u64);
         println!("{} Running data targets...", style.apply_to("[1/2]"));
 
-        let (in_dir, out_dir) = setup(config, config.extract.prefix.as_deref(), "data".as_ref())
+        let (in_dir, out_dir) = setup(config, config.user.prefix.as_deref(), "data".as_ref())
             .expect("Could not prep in/out dirs");
 
-        for item in &config.extract.files {
+        let extractor = UserExtractor::new(&config.tools.user_extractor).with_input_prefix(&in_dir);
+        let targets = get_file_targets(&in_dir, &config.user.files);
+
+        let progress = ProgressBar::new(targets.len() as u64);
+
+        targets.into_par_iter().for_each(|in_path| {
             progress.inc(1);
 
-            let glob = Glob::new(item).expect(&format!("Invalid glob of file path {}", item));
-
-            for entry in glob.walk(&in_dir) {
-                let entry = entry.expect("Invalid entry");
-
-                let in_path = entry.path();
-                let out_path = out_dir
-                    .join(in_path.file_name().unwrap())
-                    .with_extension("")
-                    .with_extension("json");
-
-                let mut data_indexes: Vec<u8> = vec![0];
-
-                if let Some(rules) = config.extract.rules.get(item) {
-                    if let Some(regex) = &rules.match_regex {
-                        if regex.is_match(in_path.to_str().unwrap()) {
-                            if let Some(indexes) = &rules.rsz_indexes {
-                                data_indexes = indexes.to_vec();
-                            }
-                        }
-                    }
-                }
-
-                for index in &data_indexes {
-                    let out_path = if data_indexes.len() > 1 && *index > 0 {
-                        let mut name = out_path.file_stem().unwrap().to_str().unwrap().to_string();
-                        name.push('_');
-                        name.push_str(&index.to_string());
-
-                        out_path.with_file_name(name).with_extension("json")
-                    } else {
-                        out_path.to_owned()
-                    };
-
-                    let args = [
-                        in_path.to_str().unwrap(),
-                        out_path.to_str().unwrap(),
-                        &index.to_string(),
-                    ];
-
-                    let success = command::exec(&config.tools.data_extractor, args);
-
-                    if !success {
-                        panic!("Failed to execute previous command.");
-                    }
-                }
-            }
-        }
-
-        progress.finish_and_clear();
-    }
-
-    if !cli.skip_translations {
-        let progress = ProgressBar::new(config.translations.files.len() as u64);
-        println!("{} Running translation targets...", style.apply_to("[2/2]"));
-
-        let (in_dir, out_dir) = setup(
-            config,
-            config.translations.prefix.as_deref(),
-            "translations".as_ref(),
-        )
-        .expect("Could not prep in/out dirs");
-
-        for item in &config.translations.files {
-            progress.inc(1);
-
-            let in_path = in_dir.join(item);
-            let success = command::exec(
-                &config.tools.msg_extractor,
-                ["-i", &in_path.to_string_lossy(), "-m", "json"],
-            );
-
-            if !success {
-                panic!("Failed to execute previous command.");
-            }
-
-            let tool_out_path = in_path.with_extension("23.json");
             let out_path = out_dir
                 .join(in_path.file_name().unwrap())
                 .with_extension("")
                 .with_extension("json");
 
-            match fs::copy(&tool_out_path, &out_path) {
-                Err(_) => panic!("Could not copy {tool_out_path:?} to {out_path:?}"),
-                _ => (),
-            };
-        }
+            let data_indexes = config
+                .user
+                .get_matching_rule(in_path.to_str().unwrap())
+                .map(|v| v.rsz_indexes.to_owned())
+                .unwrap_or_else(|| vec![0]);
+
+            extractor
+                .run_indexes(&in_path, &out_path, &data_indexes)
+                .expect("Could not extract");
+        });
 
         progress.finish_and_clear();
     }
+
+    if !cli.skip_translations {
+        println!("{} Running translation targets...", style.apply_to("[2/2]"));
+
+        let (in_dir, out_dir) = setup(
+            config,
+            config.msg.prefix.as_deref(),
+            "translations".as_ref(),
+        )
+        .expect("Could not prep in/out dirs");
+
+        let extractor = MsgExtractor::new(&config.tools.msg_extractor).with_input_prefix(&in_dir);
+        let targets = get_file_targets(&in_dir, &config.msg.files);
+
+        let progress = ProgressBar::new(targets.len() as u64);
+
+        targets.into_par_iter().for_each(|in_path| {
+            progress.inc(1);
+
+            let out_path = out_dir
+                .join(in_path.file_name().unwrap())
+                .with_extension("")
+                .with_extension("json");
+
+            extractor
+                .run(&in_path, &out_path)
+                .expect("Could not execute command");
+        });
+
+        progress.finish_and_clear();
+    }
+
+    Ok(())
+}
+
+fn get_file_targets(prefix: &Path, items: &[String]) -> Vec<PathBuf> {
+    items
+        .par_iter()
+        .flat_map(|item| -> Vec<_> {
+            let glob = Glob::new(item).expect(&format!("Invalid glob of file path {}", item));
+            glob.walk(prefix)
+                .flat_map(|v| v.and_then(|v| Ok(v.into_path())))
+                .collect()
+        })
+        .collect()
 }
